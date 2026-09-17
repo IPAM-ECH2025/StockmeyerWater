69014e36a261de355d3c58b084fcda96cc5867be
Jason Landini <landinjm@umich.edu>
2026-09-17 17:18:02 -0400

    fixes

--- src/functions.hpp
+++ src/functions.hpp
@@ -2,23 +2,6 @@
 
 #include "parameters.hpp"
 #include <Kokkos_Core.hpp>
-#include <array>
-
-// TODO: Use this
-template <typename RealType>
-KOKKOS_INLINE_FUNCTION std::array<RealType, 2> angle_to_vec(RealType theta) {
-  using Kokkos::cos;
-  using Kokkos::sin;
-  return {cos(theta), sin(theta)};
-}
-
-// TODO: Use this
-template <typename RealType>
-KOKKOS_INLINE_FUNCTION std::array<RealType, 2> angle_to_perp(RealType theta) {
-  using Kokkos::cos;
-  using Kokkos::sin;
-  return {-sin(theta), cos(theta)};
-}
 
 template <typename RealType>
 KOKKOS_INLINE_FUNCTION RealType japanese_bracket(RealType x) {
@@ -30,17 +13,17 @@ template <typename RealType, typename ViewType>
 KOKKOS_FUNCTION RealType compute_integral(const ViewType &f) {
   // NOTE: This only works for 3D views
   // NOTE: This only works for uniform grids
+  using P = Parameters<RealType>;
+
   RealType sum = 0.0;
 
   Kokkos::parallel_reduce(
       "compute_integral",
-      Kokkos::MDRangePolicy<Kokkos::Rank<3>>(
-          {0, 0, 0}, {Parameters<RealType>::N_x, Parameters<RealType>::N_y,
-                      Parameters<RealType>::N_z}),
-      KOKKOS_LAMBDA(const int &i, const int &j, const int &k,
+      Kokkos::MDRangePolicy<Kokkos::Rank<3>>({0, 0, 0},
+                                             {P::N_y, P::N_theta, P::N_omega}),
+      KOKKOS_LAMBDA(const int i, const int j, const int k,
                     RealType &local_sum) { local_sum += f(i, j, k); },
       sum);
 
-  return sum * Parameters<RealType>::dx * Parameters<RealType>::dy *
-         Parameters<RealType>::dz;
+  return sum * P::dy * P::dtheta * P::domega;
 }
--- src/output.hpp
+++ src/output.hpp
@@ -8,10 +8,10 @@
 #include <string>
 
 template <typename RealType, typename ViewType3D, typename ViewType1D>
-void write_vtu(const ViewType3D &f, const ViewType1D &x, const ViewType1D &y,
-               const ViewType1D &z, const std::string &filename,
-               RealType time = 0.0, int increment = 0,
-               const std::string &output_dir = "outputs/") {
+void write_vtu(const ViewType3D &f, const ViewType1D &y,
+               const ViewType1D &theta, const ViewType1D &omega,
+               const std::string &filename, RealType time = 0.0,
+               int increment = 0, const std::string &output_dir = "outputs/") {
   // NOTE: This only works for 3D views
   // NOTE: Casts to float32
   using P = Parameters<RealType>;
@@ -24,18 +24,18 @@ void write_vtu(const ViewType3D &f, const ViewType1D &x, const ViewType1D &y,
   }
   fstream << std::setprecision(std::numeric_limits<float>::max_digits10);
 
-  const auto _N_x = P::N_x;
   const auto _N_y = P::N_y;
-  const auto _N_z = P::N_z;
+  const auto _N_theta = P::N_theta;
+  const auto _N_omega = P::N_omega;
 
   fstream << "<?xml version=\"1.0\"?>\n";
   fstream << "<VTKFile type=\"RectilinearGrid\" version=\"0.1\" "
              "byte_order=\"LittleEndian\">\n";
 
   fstream << "  <RectilinearGrid WholeExtent=\""
-          << "0 " << _N_x - 1 << " "
           << "0 " << _N_y - 1 << " "
-          << "0 " << _N_z - 1 << "\">\n";
+          << "0 " << _N_theta - 1 << " "
+          << "0 " << _N_omega - 1 << "\">\n";
 
   fstream << "    <FieldData>\n";
   fstream << "      <DataArray type=\"Float64\" Name=\"Time\" "
@@ -49,16 +49,16 @@ void write_vtu(const ViewType3D &f, const ViewType1D &x, const ViewType1D &y,
   fstream << "    </FieldData>\n";
 
   fstream << "    <Piece Extent=\""
-          << "0 " << _N_x - 1 << " "
           << "0 " << _N_y - 1 << " "
-          << "0 " << _N_z - 1 << "\">\n";
+          << "0 " << _N_theta - 1 << " "
+          << "0 " << _N_omega - 1 << "\">\n";
 
   fstream << "      <PointData Scalars=\"f\">\n";
   fstream << "        <DataArray type=\"Float32\" Name=\"f\" "
              "NumberOfComponents=\"1\" format=\"ascii\">\n";
-  for (int k = 0; k < _N_z; ++k) {
-    for (int j = 0; j < _N_y; ++j) {
-      for (int i = 0; i < _N_x; ++i) {
+  for (int k = 0; k < _N_omega; ++k) {
+    for (int j = 0; j < _N_theta; ++j) {
+      for (int i = 0; i < _N_y; ++i) {
         fstream << "          " << (float)f(i, j, k) << "\n";
       }
     }
@@ -69,20 +69,20 @@ void write_vtu(const ViewType3D &f, const ViewType1D &x, const ViewType1D &y,
   fstream << "      <Coordinates>\n";
   fstream << "        <DataArray type=\"Float32\" Name=\"XCoordinates\" "
              "format=\"ascii\">\n";
-  for (int i = 0; i < _N_x; ++i) {
-    fstream << "          " << (float)x(i) << "\n";
+  for (int i = 0; i < _N_y; ++i) {
+    fstream << "          " << (float)y(i) << "\n";
   }
   fstream << "        </DataArray>\n";
   fstream << "        <DataArray type=\"Float32\" Name=\"YCoordinates\" "
              "format=\"ascii\">\n";
-  for (int j = 0; j < _N_y; ++j) {
-    fstream << "          " << (float)y(j) << "\n";
+  for (int j = 0; j < _N_theta; ++j) {
+    fstream << "          " << (float)theta(j) << "\n";
   }
   fstream << "        </DataArray>\n";
   fstream << "        <DataArray type=\"Float32\" Name=\"ZCoordinates\" "
              "format=\"ascii\">\n";
-  for (int k = 0; k < _N_z; ++k) {
-    fstream << "          " << (float)z(k) << "\n";
+  for (int k = 0; k < _N_omega; ++k) {
+    fstream << "          " << (float)omega(k) << "\n";
   }
   fstream << "        </DataArray>\n";
   fstream << "      </Coordinates>\n";
